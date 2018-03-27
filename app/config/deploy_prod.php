c7eea4dbf9d205b37b67b4bc12714551eaddac06
Mathieu ALLAIN <mathieu.allain@amadeus.com>
2018-03-28 00:22:14 +0200

    code: add deployment automation

---
+++ app/config/deploy_prod.php
@@ -0,0 +1,33 @@
+<?php
+
+use EasyCorp\Bundle\EasyDeployBundle\Deployer\DefaultDeployer;
+
+return new class extends DefaultDeployer
+{
+    public function configure()
+    {
+        return $this->getConfigBuilder()
+            // SSH connection string to connect to the remote server (format: user@host-or-IP:port-number)
+            ->server('scaleway_client')
+            // the absolute path of the remote server directory where the project is deployed
+            ->deployDir('/var/www/gite29')
+            // the URL of the Git repository where the project code is hosted
+//            ->repositoryUrl('git@github.com:math29/gite29.git')
+            // the repository branch to deploy
+//            ->repositoryBranch('master')
+        ;
+    }
+
+    // run some local or remote commands before the deployment is started
+    public function beforeStartingDeploy()
+    {
+        // $this->runLocal('./vendor/bin/simple-phpunit');
+    }
+
+    // run some local or remote commands after the deployment is finished
+    public function beforeFinishingDeploy()
+    {
+        // $this->runRemote('{{ console_bin }} app:my-task-name');
+        // $this->runLocal('say "The deployment has finished."');
+    }
+};
